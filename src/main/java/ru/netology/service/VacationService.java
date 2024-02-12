00d64b4ccb35ae3275c7de1f9983518f058eb0fd
Nikita Spiridonov <spiridonov.n.i@yandex.ru>
2024-02-12 22:50:31 +0300

    Initial Commit

---
+++ src/main/java/ru/netology/service/VacationService.java
@@ -0,0 +1,19 @@
+package ru.netology.service;
+
+public class VacationService {
+    public int calculateVacationMonths(int income, int expenses, int threshold) {
+        int vacationMonths = 0;
+        int savings = 0;
+
+        for (int month = 1; month <= 12; month++) {
+            if (savings >= threshold) {
+                vacationMonths++;
+                savings = savings - expenses - savings * 2 / 3;
+            } else {
+                savings += income - expenses;
+            }
+        }
+
+        return vacationMonths;
+    }
+}
